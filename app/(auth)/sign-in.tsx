625ff142a416225973289bc16ce30061193dbdfb
Amos Makupe <amosmakupe8@gmail.com>
2026-04-06 05:51:25 +0200

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+import {Link} from "expo-router";
+
+const SignIn = () => {
+    return (
+        <View>
+            <Text>SignIn</Text>
+            <Link href="/(auth)/sign-up">Sign In</Link>
+        </View>
+    )
+}
+export default SignIn
